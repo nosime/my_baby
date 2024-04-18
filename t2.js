e1b36813fe2fd7c19bb0d2f777b3fbb9c322aecd
nosime <voson0404@gmail.com>
2024-04-18 10:08:51 +0700

    first commit

--- t2.js
+++ t2.js
@@ -1,38 +1,44 @@
-function fadeIn(){
-    var text = $('.content').text().replace(/^\s+|\s+$/g,'');
+var audio = null;
+var isAudioPlaying = false;
+
+function fadeIn() {
+    var text = $('.content').text().replace(/^\s+|\s+$/g, '');
     var length = text.length;
     var i = 0;
     var txt;
     var html = [];
     var sp = 4;
-    for(;i<length;i+=sp) {
-        txt = text.substring(i,i+sp);
+    for (; i < length; i += sp) {
+        txt = text.substring(i, i + sp);
         html.push('<span class="c animated">' + txt + '</span>');
     }
     $('.content').removeClass('c').html(html.join(''));
 
-    for(i = 0,length = html.length;i<length;i++) {
-        !function(i){
-            setTimeout(function(){
+    for (i = 0, length = html.length; i < length; i++) {
+        !function (i) {
+            setTimeout(function () {
                 $('.content').find('.animated').eq(i).addClass('fadeIn');
-            },i*400);
+            }, i * 400);
         }(i);
     }
-};
+    playAudio();
+}
 
 function playAudio() {
-    var audio = new Audio("sound.mp3");
+    if (audio && isAudioPlaying) {
+        return; // Nếu audio đang phát thì không chạy lại
+    }
+    audio = new Audio("sound.mp3");
+    audio.onended = function () {
+        isAudioPlaying = false; // Đặt lại cờ khi audio phát xong
+    };
     audio.play();
+    isAudioPlaying = true; // Đặt cờ là audio đang phát
 }
 
-// Sử dụng hàm playAudio để chạy một tệp âm thanh cụ thể
-
-
-
-document.querySelector(".content").onclick=()=>{
-    document.querySelector("#heart").hidden=false
-    document.querySelector("body").style.backgroundColor="#542246"
-    document.querySelector("#heart").hidden=false
-    fadeIn()
-    playAudio()
-}
+document.querySelector(".content").onclick = () => {
+    document.querySelector("#heart").hidden = false;
+    document.querySelector("body").style.backgroundColor = "#542246";
+    document.querySelector("#heart").hidden = false;
+    fadeIn();
+};
